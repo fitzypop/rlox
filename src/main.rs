75fe156eaa9ac77b37ff35bef87517f0e99fee92
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-01-26 22:43:55 -0700

    remove termion infavor of stdin.read_line()

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,55 @@
-use anyhow::Result;
+use anyhow::{Context, Ok, Result};
 use std::{
     env,
-    io::{stdin, stdout, Write},
+    fs::File,
+    io::{stdin, stdout, BufRead, Read, StdoutLock, Write},
 };
-use termion::input::TermRead;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
     match args.len() {
         0 => run_repl(),
-        1 => run_w_arg(&args[0]),
+        1 => run_file(&args[0]),
         _ => todo!(),
     }
 }
 
 fn run_repl() -> Result<()> {
-    let stdout = stdout();
-    let mut stdout = stdout.lock();
-    let stdin = stdin();
-    let mut stdin = stdin.lock();
+    let mut stdout = stdout().lock();
+    let mut stdin = stdin().lock();
 
-    stdout.write_all(b"password: ")?;
+    loop {
+        stdout.write_all(b">> ")?;
+        stdout.flush()?;
 
-    stdout.flush()?;
-
-    let pass = stdin.read_passwd(&mut stdout);
-
-    if let Ok(Some(pass)) = pass {
-        stdout.write_all(b"\n")?;
-        stdout.write_all(pass.as_bytes())?;
-        stdout.write_all(b"\n")?;
-    } else {
-        stdout.write_all(b"Error\n")?;
+        let mut line = String::new();
+        let _by = stdin.read_line(&mut line)?;
+        let line_in = line.trim();
+        if line_in == "exit" {
+            return Ok(());
+        }
+        run(line_in.to_string(), &mut stdout)?;
     }
+}
+
+fn run_file(arg: &str) -> Result<()> {
+    if arg == "-h" || arg == "--help" {
+        println!("");
+        return Ok(());
+    }
+    let mut stdout = stdout().lock();
+
+    let mut file = File::open(arg).with_context(|| format!("Could not open file {}", arg))?;
+
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)?;
+
+    run(buf, &mut stdout)?;
     Ok(())
 }
 
-fn run_w_arg(arg: &str) -> Result<()> {
-    if arg == "-h" || arg == "--help" {
-        println!("");
-    }
-
-    println!("Cli Arg: {}", arg);
-
+fn run(source: String, stdout: &mut StdoutLock<'_>) -> Result<()> {
+    stdout.write_all(source.as_bytes())?;
+    stdout.write_all(b"\n")?;
     Ok(())
 }
